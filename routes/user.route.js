2ce627b6276fcf6bb4807e766a60bceac6d0d9a9
Yashoda826 <pothineniyashoda@gmail.com>
2021-08-23 12:18:07 +0530

    code added

---
+++ routes/user.route.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const userRouter =express.Router();
+
+const jwt = require('jsonwebtoken');
+
+const userController = require('../controllers/user.controller');
+
+
+var authMiddleware = function(req,res,next){
+
+    console.log(req.headers);
+
+     if(req.headers.authorization){
+            var token = req.headers.authorization.split(' ')[1];
+
+             if(token != "null" && token != undefined){
+                
+                try{                
+               var payload=jwt.verify(token, process.env.JWT_SECRET);
+               
+               console.log("payload",payload);
+               console.log("req.body._id",req.body._id);
+               if(payload.subject == req.body._id){
+                   next();
+               }
+               else
+               {
+                res.status(401).send("unauthorized access!");
+               }
+            }
+            catch(error){
+ 
+                res.status(401).send({error:error.message});
+
+            }
+             }
+             else
+             {
+                res.status(401).send("unauthorized access!");
+             }
+     }
+     else
+     {
+        res.status(401).send("unauthorized access!");
+     }
+}
+
+userRouter.post('/forgotpassword',userController.forgotPassword);
+
+userRouter.post('/register',userController.register);
+
+userRouter.post('/login', userController.login);
+
+userRouter.put('/changePassword',userController.changePassword);
+// userRouter.put('/changePassword',userController.changePassword);
+userRouter.patch("/updateRole", userController.updateRole);
+
+userRouter.get('/allUsers', userController.getAllUsers);
+
+userRouter.put('/resetPassword' ,userController.resetPassword);
+
+
+module.exports = userRouter;
+
+
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoiNjBiNzUzZDY3ZjYwN2Q3YWVhOTdkYjc1IiwiaWF0IjoxNjIyNzE0MjQ4fQ.ITTA_iWQVZ1fJOoX3O87Wvhrv1XH48JBFkbjQZUnlVM
+
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoiNjBiOGE5MDlmODRhYWU0OTgzNzJiMzg0IiwiaWF0IjoxNjIyNzE0NjkxfQ.JeukP8Ide2CW9iAFH71RcNEnOUSgnIYeRewTldh8ijM
